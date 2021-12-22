b089f3a0cbca3e07a32b1bd628f36fda5117775e
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-22 19:16:42 +0100

    Display the fetched data dynamically

--- src/index.js
+++ src/index.js
@@ -16,11 +16,28 @@ fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
     gameId = json.result.replace('Game with ID: ', '').replace(' added.', '');
   });
 
+const display = (result) => {
+  const list = document.getElementById('list');
+  list.innerHTML = '';
+  for (let i = 0; i < result.length; i += 1) {
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-li-0');
+    if (i % 2 === 0) {
+      listItem.classList.add('list-li-0');
+    } else {
+      listItem.classList.add('list-li-1');
+    }
+    listItem.innerHTML = `${result[i].user}: ${result[i].score}`;
+    list.appendChild(listItem);
+  }
+};
+
 const component = () => {
   const element = document.createElement('div');
 
   const refresh = document.getElementById('refresh');
   refresh.addEventListener('click', () => {
+    let result;
     fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
       method: 'GET',
       headers: {
@@ -29,7 +46,8 @@ const component = () => {
     })
       .then((response) => response.json())
       .then((json) => {
-        console.log(json);
+        result = json.result;
+        display(result);
       });
   });
 
@@ -37,8 +55,6 @@ const component = () => {
   submit.addEventListener('click', () => {
     const name = document.getElementById('name').value;
     const score = document.getElementById('score').value;
-    console.log(name);
-    console.log(score);
     fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
       method: 'POST',
       body: JSON.stringify({
