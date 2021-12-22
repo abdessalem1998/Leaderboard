d18b435958d542c070db4822cba60f35a8a5d6c8
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-22 15:34:00 +0100

    Script for create a new game and get the ID of the game

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import './style.css';
 
+// create the game ID
+let gameId = 'Still loading';
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'Dib game',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => {
+    gameId = json.result.replace('Game with ID: ', '').replace(' added.', '');
+  });
+console.log(gameId);
 const component = () => {
   const element = document.createElement('div');
   return element;
