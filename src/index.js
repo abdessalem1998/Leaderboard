9ca50bf351a37369f229f2d2b2afe33b9d8b4061
abdessalem1998 <89970442+abdessalem1998@users.noreply.github.com>
2021-12-22 17:30:28 +0100

    Implement the form "Submit" button (sending data to the API).

--- src/index.js
+++ src/index.js
@@ -33,6 +33,28 @@ const component = () => {
       });
   });
 
+  const submit = document.getElementById('submit');
+  submit.addEventListener('click', () => {
+    const name = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    console.log(name);
+    console.log(score);
+    fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`, {
+      method: 'POST',
+      body: JSON.stringify({
+        user: `${name}`,
+        score: `${score}`,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+      });
+  });
+
   return element;
 };
 
